b3995b0fc334512fca3fa8cfb650cbf55f23d312
Casper <32563753+MegaRandomCake@users.noreply.github.com>
2018-05-11 12:36:58 +0200

    This code works
    //TODO write to file and save picture

--- init.c
+++ init.c
@@ -27,6 +27,7 @@ int main(){
 		IplImage* endres = cvCreateImage(cvSize(w, h), 8, 1);
 		IplImage* endendres = cvCreateImage(cvSize(w, h), 8, 1);
 		printf("ready\n");
+		
 
 		while(1)
 		{
@@ -35,37 +36,16 @@ int main(){
 				printf("Stream not found");
 				break;
 			}
-			cvCvtColor(res, previous, CV_BGR2GRAY);
+			cvCopy(res, previous, CV_8UC1);
 			cvCvtColor(current, res, CV_BGR2GRAY);
-			//for(int i = 0; i < w; i++){
-				//for(int j = 0; j < h; j++){
-					//int k = CV_IMAGE_ELEM(res, unsigned char, i, j); //Returns an integer on the greyscale from pixel x,y.
-					//int l = CV_IMAGE_ELEM(previous, unsigned char, i, j);
-					//if(abs(k-l) != 0){
-						//m++;}
-				//}
-			//}
-
-				//m = (m / (h * w)) * 100;
-				
-				//if(m >= 0){
 				cvAbsDiff(previous, res, endres);
-			    cvThreshold(endres, endendres, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+			    cvThreshold(endres, endendres, 50, 255, CV_THRESH_BINARY | CV_THRESH_TOZERO);
 				int m = cvCountNonZero(endendres);
 					printf("%i\n", m);
 					cvSaveImage("apreviouspic.jpg", previous, 0);
 					cvSaveImage("arespic.png", res, 0);
 					cvSaveImage("aendrespic.png", endres, 0 );
 					cvSaveImage("aendendrespic.png", endendres, 0);
-				//time(&rawtime);
-				//info = localtime(&rawtime);
-				//YYYY-MM-DD hh:mm:ss
-				//strftime(c_time_string, 20, "%Y-%m-%d %X", info);
-				//fprintf(log, c_time_string);
-				//cvCopy(res, previous, CV_8UC1);
-				//sleep(5);
-			//}
-			//m=0;
 		}
 		cvReleaseCapture(&capture);
 	}
