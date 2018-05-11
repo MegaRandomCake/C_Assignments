c685e94d4793bd7cb1e50e7e5dd8fbc47e6ed7f8
Casper <32563753+MegaRandomCake@users.noreply.github.com>
2018-05-11 23:51:53 +0200

    Removed the last Win-OS stuff

--- init.c
+++ init.c
@@ -5,10 +5,10 @@ typedef struct {
 	int length, linenumber;
 } Point;
 
-//variables initializiation 
+//variables initializiation
 //NumberATo counts special character when counting o's and numberATx counts special characters when counting x's
 //MAXIMUM is current highest continious line of x and o characters for where to break the chain
-int curr = 0, prev = 0, NumberX = 0, NumberO = 0, NumberATo = 0, NumberATx = 0, MAXIMUM = 0, 
+int curr = 0, prev = 0, NumberX = 0, NumberO = 0, NumberATo = 0, NumberATx = 0, MAXIMUM = 0,
 linecounter = -1, prevLineCounter = 0, switchcounter = 0, totallinecounter = 0, breaker = 1;
 Point maks;
 
@@ -64,17 +64,17 @@ void TheMagic(){
 }
 
 int main(void) {
-	
+
 	FILE *inputfile;
-	inputfile = fopen(NECKLACE.dat, "r");
+	inputfile = fopen("NECKLACE.dat", "r");
 
 	if(inputfile){
 		//specifies the file for output
 		FILE *outputfile;
 		//w means the file is created. If another file exists with the same name it is deleted.
-		outputfile = fopen(NECKLACE.sol, "w"); 
+		outputfile = fopen("NECKLACE.sol", "w");
 		totallinecounter = 0;
-		
+
 		//sets variables values back to first assigned values and goes back to start of file
 		while(breaker != -1){
 			curr = 0, prev = 0, NumberX = 0, NumberO = 0,
@@ -92,7 +92,7 @@ int main(void) {
 		totallinecounter = temp;
 		linecounter = -1;
 		switchcounter = 0;
-		
+
 		//runs TheMagic() function while current character hasnt reached EOF (End of file) and current character isnt "10"
 		while(((curr = getc(inputfile)) != EOF) && curr != 10){
 			TheMagic();
@@ -104,18 +104,16 @@ int main(void) {
 		//Closes the files and opens the .sol file
 		fclose(outputfile);
 		fclose(inputfile);
-		system(outputname);
 	}
 	//Creates a file and shows it to the user.
 	else{
-		inputfile = fopen(NECKLACE.dat, "a");
+		inputfile = fopen("NECKLACE.dat", "a");
 		//prints to the file "inputfile"
 		fprintf(inputfile, "This file have been created on your desktop, to draw a beautiful necklace consisting of 'x', 'o' and '@'."
 				"\nPlease make a necklace, save it and restart the application."
 				"\nRemember to delete this .dat file and the .sol result file when done with the application.");
 		//closes the file
 		fclose(inputfile);
-		system(inputname);
 	}
 
 	return 0;
